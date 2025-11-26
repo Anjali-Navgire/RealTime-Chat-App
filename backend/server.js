1d1fc1d6c639e022eeceae925de15ff8741b413b
Anjali-Navgire <anjalinavgire3109@gmail.com>
2025-11-26 20:00:03 +0530

    URI changed

--- backend/server.js
+++ backend/server.js
@@ -6,18 +6,23 @@ import { Server } from "socket.io";
 const app = express();
 const server = createServer(app);
 
-const io = new Server(server,{
-  cors:{
-    origin:'http://localhost:5173',
-  },
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:5173",
+      "https://chat-app-6kwf.onrender.com"   
+    ],
+    methods: ["GET", "POST"],
+    credentials: true
+  }
 })
- 
+
 const ROOM = 'group'
 
 io.on('connection', (socket) => {
   console.log('a user connected', socket.id);
 
-  socket.on('joinRoom',async (userName)=>{
+  socket.on('joinRoom', async (userName) => {
     console.log(`${userName} is joining the group.`)
 
     await socket.join(ROOM);
@@ -27,16 +32,16 @@ io.on('connection', (socket) => {
 
   });
 
-  socket.on('chatMessage',(msg)=>{
-    socket.to(ROOM).emit('chatMessage',msg);
+  socket.on('chatMessage', (msg) => {
+    socket.to(ROOM).emit('chatMessage', msg);
   });
 
-  socket.on('typing',(userName)=>{
-    socket.to(ROOM).emit('typing',userName);
+  socket.on('typing', (userName) => {
+    socket.to(ROOM).emit('typing', userName);
   });
 
-  socket.on('stopTyping',(userName)=>{
-    socket.to(ROOM).emit('stopTyping',userName);
+  socket.on('stopTyping', (userName) => {
+    socket.to(ROOM).emit('stopTyping', userName);
   });
 });
 
